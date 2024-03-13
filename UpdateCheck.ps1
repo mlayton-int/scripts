ab7ab8ace1d0857d2ffaae1f4b6e5c25c20fc9b6
mlayton-int <162744755+mlayton-int@users.noreply.github.com>
2024-03-13 12:59:35 -0400

    Module actually works now

--- UpdateCheck.ps1
+++ UpdateCheck.ps1
@@ -1,2 +1,8 @@
 $modules = "Microsoft.Graph", "Microsoft.Graph.Beta"
 
+foreach ($item in $modules) {
+   $latest = (Find-Module -name $item).version 
+   $current = (Get-InstalledModule -name $item).version
+
+   Write-Host ("Latest version of $item is $latest, currently installed is $current")
+}
